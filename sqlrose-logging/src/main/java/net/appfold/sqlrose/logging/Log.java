00a7f0c44b8094a9fff447cff621cfc99154a24f
Octavian Nita <octavian.nita@gmail.com>
2019-05-13 14:19:01 +0200

    Simplify the logging module
     * to only contain configuration and its entry point

--- sqlrose-logging/src/main/java/net/appfold/sqlrose/logging/Log.java
+++ sqlrose-logging/src/main/java/net/appfold/sqlrose/logging/Log.java
@@ -1,25 +1,26 @@
 package net.appfold.sqlrose.logging;
 
 import me.nallar.whocalled.WhoCalled;
-import org.slf4j.*;
+import org.slf4j.Logger;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import static org.slf4j.Logger.ROOT_LOGGER_NAME;
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
- * Entry point for various <a href="https://www.slf4j.org/">SLF4J</a>-based, logging-related utilities.
- * <p/>
- * The various methods using format strings pass them and the rest of the arguments directly to SLF4J.
+ * Entry point for logging-related configuration and (<a href="https://www.slf4j.org/">SLF4J</a>-based) utilities.
  *
- * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
+ * @author Octavian Nita (https://github.com/octavian-nita/)
  * @version 2.0, Nov 8, 2017
  */
-public interface Log {
+public class Log {
 
     /**
-     * Performs additional logging initialization, like {@link #routeJulToSlf4j() adapting j.u.l to log}, etc.
+     * Performs additional logging configuration, like {@link #routeJulToSlf4j() adapting j.u.l to logFor}, etc.
      * <p/>
      * Should probably be invoked early during the initialization phase of the application.
      */
-    static void init() {
+    public static void config() {
         routeJulToSlf4j();
     }
 
@@ -27,12 +28,12 @@ public interface Log {
      * Route any j.u.l logging request to SLF4J.
      *
      * @see <a href="http://blog.cn-consult.dk/2009/03/bridging-javautillogging-to-slf4j.html">Bridging
-     * java.util.logging to SLF4J</a>
+     *     java.util.logging to SLF4J</a>
      * @see <a href="http://stackoverflow.com/questions/9117030/jul-to-slf4j-bridge">JUL to SLF4J Bridge</a>
      * @see <a href="https://logback.qos.ch/manual/configuration.html#LevelChangePropagator">LevelChangePropagator
-     * configuration</a>
+     *     configuration</a>
      */
-    static void routeJulToSlf4j() {
+    public static void routeJulToSlf4j() {
         // We might not need this if we're installing a LevelChangePropagator
         java.util.logging.LogManager.getLogManager().reset();
 
@@ -42,163 +43,27 @@ public interface Log {
         java.util.logging.Logger.getGlobal().setLevel(java.util.logging.Level.FINE /* -> DEBUG in SLF4J */);
     }
 
-    default void d(final Object source, final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(source);
-        slf4j.debug("", throwable);
-        slf4j.debug(fmt, args);
-    }
+    public static Logger log() { return logFor(null); }
 
-    default void d(final Object source, final String msg, final Throwable throwable) {
-        log(source).debug(msg, throwable);
-    }
-
-    default void d(final Object source, final String fmt, final Object... args) {
-        log(source).debug(fmt, args);
-    }
-
-    default void d(final Object source, final Throwable throwable) {
-        log(source).debug("", throwable);
-    }
-
-    default void d(final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(null);
-        slf4j.debug("", throwable);
-        slf4j.debug(fmt, args);
-    }
-
-    default void d(final String msg, final Throwable throwable) {
-        log(null).debug(msg, throwable);
-    }
-
-    default void d(final String fmt, final Object... args) {
-        log(null).debug(fmt, args);
-    }
-
-    default void d(final Throwable throwable) {
-        log(null).debug("", throwable);
-    }
-
-    default void i(final Object source, final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(source);
-        slf4j.info("", throwable);
-        slf4j.info(fmt, args);
-    }
-
-    default void i(final Object source, final String msg, final Throwable throwable) {
-        log(source).info(msg, throwable);
-    }
-
-    default void i(final Object source, final String fmt, final Object... args) {
-        log(source).info(fmt, args);
-    }
-
-    default void i(final Object source, final Throwable throwable) {
-        log(source).info("", throwable);
-    }
-
-    default void i(final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(null);
-        slf4j.info("", throwable);
-        slf4j.info(fmt, args);
-    }
-
-    default void i(final String msg, final Throwable throwable) {
-        log(null).info(msg, throwable);
-    }
-
-    default void i(final String fmt, final Object... args) {
-        log(null).info(fmt, args);
-    }
-
-    default void i(final Throwable throwable) {
-        log(null).info("", throwable);
-    }
-
-    default void w(final Object source, final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(source);
-        slf4j.warn("", throwable);
-        slf4j.warn(fmt, args);
-    }
-
-    default void w(final Object source, final String msg, final Throwable throwable) {
-        log(source).warn(msg, throwable);
-    }
-
-    default void w(final Object source, final String fmt, final Object... args) {
-        log(source).warn(fmt, args);
-    }
-
-    default void w(final Object source, final Throwable throwable) {
-        log(source).warn("", throwable);
-    }
-
-    default void w(final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(null);
-        slf4j.warn("", throwable);
-        slf4j.warn(fmt, args);
-    }
-
-    default void w(final String msg, final Throwable throwable) {
-        log(null).warn(msg, throwable);
-    }
-
-    default void w(final String fmt, final Object... args) {
-        log(null).warn(fmt, args);
-    }
-
-    default void w(final Throwable throwable) {
-        log(null).warn("", throwable);
-    }
-
-    default void e(final Object source, final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(source);
-        slf4j.error("", throwable);
-        slf4j.error(fmt, args);
-    }
-
-    default void e(final Object source, final String msg, final Throwable throwable) {
-        log(source).error(msg, throwable);
-    }
-
-    default void e(final Object source, final String fmt, final Object... args) {
-        log(source).error(fmt, args);
-    }
-
-    default void e(final Object source, final Throwable throwable) {
-        log(source).error("", throwable);
-    }
-
-    default void e(final Throwable throwable, final String fmt, final Object... args) {
-        final Logger slf4j = log(null);
-        slf4j.error("", throwable);
-        slf4j.error(fmt, args);
-    }
-
-    default void e(final String msg, final Throwable throwable) {
-        log(null).error(msg, throwable);
-    }
-
-    default void e(final String fmt, final Object... args) {
-        log(null).error(fmt, args);
-    }
-
-    default void e(final Throwable throwable) {
-        log(null).error("", throwable);
-    }
-
-    default Logger log() {
-        return log(null);
-    }
-
-    default Logger log(final Object source) {
+    /**
+     * @param source if {@code null}, a {@link Logger} instance corresponding to the calling class is retrieved
+     * @return a {@link Logger} instance <em>corresponding</em> to the provided {@code source} argument
+     */
+    public static Logger logFor(final Object source) {
         if (source == null) {
-            return LoggerFactory.getLogger(WhoCalled.$.getCallingClass());
-        } else if (source instanceof String) {
-            return LoggerFactory.getLogger((String) source);
+            try {
+                return getLogger(WhoCalled.$.getCallingClass(2));
+            } catch (Throwable throwable) {
+                return getLogger(ROOT_LOGGER_NAME);
+            }
         } else if (source instanceof Class<?>) {
-            return LoggerFactory.getLogger((Class<?>) source);
+            return getLogger((Class<?>) source);
+        } else if (source instanceof String) {
+            return getLogger((String) source);
+        } else if (source instanceof Logger) {
+            return (Logger) source;
         } else {
-            return LoggerFactory.getLogger(source.getClass());
+            return getLogger(source.getClass());
         }
     }
 }
