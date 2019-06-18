73b6fdccd9e8d1c603527f3a643b70ab3c554a38
Octavian Nita <octavian.nita@gmail.com>
2019-06-18 16:20:10 +0200

    Support locale-specific instant formatting

--- sqlrose-i18n/src/main/java/net/appfold/sqlrose/i18n/I18n.java
+++ sqlrose-i18n/src/main/java/net/appfold/sqlrose/i18n/I18n.java
@@ -2,23 +2,25 @@ package net.appfold.sqlrose.i18n;
 
 import lombok.NonNull;
 
-import java.time.ZoneId;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 /**
- * Entry point/façade for a basic internationalization subsystem which:
+ * Entry point to / façade of a basic internationalization subsystem which:
  * <ul>
- * <li><em>groups</em>s several parameters which play a part in consistently internationalizing the application, e.g.,
- * {@link #getLocale() locale}, {@link java.time.format.DateTimeFormatter date/time formatter}, {@link java.time.ZoneId
- * zone}, etc.</li>
- * <li><em>outline</em>s a set of essential primitives which can be used to internationalize the application,
+ * <li><em>groups</em> parameters which play a part in consistently internationalizing the application, e.g., {@link
+ * #getLocale() locale}, {@link #getDateTimeFormatter() date/time formatter}, {@link #getZoneId() time-zone}, etc.</li>
+ * <li><em>outlines</em> a set of essential primitives which can be used to internationalize the application,
  * regardless of the actual implementation mechanism or framework employed ({@link java.util.ResourceBundle}-based, <a
- * href="https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/context/support/ReloadableResourceBundleMessageSource.html">Spring</a>-based,
- * etc.)</li>
- * <li>employs shorter names for commonly used routines e.g., {@link #t(String, Object...) t} for
- * <em>translating</em>messages, <em>l</em> for <em>localizing</em> values like dates and numbers, etc.</li>
+ * href="https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/context/support/ReloadableResourceBundleMessageSource.html">
+ * Spring</a>-based, etc.)</li>
+ * <li>employs shorter names for commonly used routines, e.g., {@link #t(String, Object...) t} for
+ * <em>translating</em> messages, {@link #l(Instant) l} for <em>localizing</em> values like dates and numbers,
+ * etc.</li>
  * </ul>
+ * {@code I18n} instances can, for example, be injected/set up to represent user profile or request-specific,
+ * (i18n-related) configuration settings.
  *
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, May 23, 2019
@@ -59,10 +61,15 @@ public interface I18n {
 
     /**
      * If no message can be found/resolved for the given {@code key} and {@code #getLocale() currently used locale},
-     * depending on the actual implementation, this method could return the key itself (if non-{@code null}), an empty
-     * string, etc.
+     * and depending on the actual implementation, this method could return the key itself (if non-{@code null}), an
+     * empty string, etc.
      *
      * @return never {@code null}
      */
     @NonNull String t(String key, Object... args);
+
+    /**
+     * @return never {@code null} (at worst, an empty string)
+     */
+    @NonNull String l(Instant instant);
 }
--- sqlrose-i18n/src/main/java/net/appfold/sqlrose/i18n/SimpleI18n.java
+++ sqlrose-i18n/src/main/java/net/appfold/sqlrose/i18n/SimpleI18n.java
@@ -1,14 +1,17 @@
 package net.appfold.sqlrose.i18n;
 
 import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
 
 import java.text.MessageFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.function.BiFunction;
+import java.util.function.*;
 
 import static java.lang.System.getProperty;
+import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 import static java.util.Collections.addAll;
+import static java.util.Optional.*;
 import static java.util.ResourceBundle.getBundle;
 import static net.appfold.sqlrose.cache.MemoizedBiFn.memoize;
 
@@ -21,10 +24,11 @@ import static net.appfold.sqlrose.cache.MemoizedBiFn.memoize;
  * <code><a href="https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/context/support/ResourceBundleMessageSource.html">
  * ResourceBundleMessageSource</a></code>, this class relies on the underlying JDK's {@link ResourceBundle}
  * implementation in combination with the JDK's standard message parsing provided by {@link MessageFormat},
- * caches by default the generated message formats for each message and can be configured concerning how both the
- * {@link #setResourceBundleSupplier(BiFunction) resource bundles} and the {@link #setMessageFormatSupplier(BiFunction)
- * message formats} are created. It also suffers from the same shortcomings as {@code ResourceBundleMessageSource} so a
- * {@code ReloadableResourceBundleMessageSource} might be a better choice if the client application is based on Spring.
+ * {@link net.appfold.sqlrose.cache.SoftCache (soft-)caches} by default the generated message formats for each message
+ * and can be configured concerning how both the {@link #setResourceBundleSupplier(BiFunction) resource bundles} and
+ * the {@link #setMessageFormatSupplier(BiFunction) message formats} are created. It also suffers from the same
+ * shortcomings as {@code ResourceBundleMessageSource} so a {@code ReloadableResourceBundleMessageSource} might be a
+ * better choice if the client application is based on Spring.
  *
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, May 23, 2019
@@ -35,8 +39,33 @@ import static net.appfold.sqlrose.cache.MemoizedBiFn.memoize;
  * @see <a href="https://stackoverflow.com/a/7355094/272939">Answer to <em>Is there a way to refer to the current type
  *     with a type variable?</em></a>
  */
-@Slf4j
 public class SimpleI18n<SELF extends SimpleI18n<SELF>> implements I18n {
+
+    /**
+     * @see <a href="https://stackoverflow.com/a/23895571/272939">Answer to <em>Fluent API with inheritance and
+     *     generics</em></a>
+     * @see <a href="https://stackoverflow.com/a/7355094/272939">Is there a way to refer to the current type with a type
+     */
+    @SuppressWarnings("unchecked")
+    protected final SELF self() {
+        return (SELF) this;
+    }
+
+    @NonNull
+    @SafeVarargs
+    protected final <T> Optional<T> firstNonNull(Supplier<T>... suppliers) {
+        if (suppliers != null) {
+            for (Supplier<T> supplier : suppliers) {
+                if (supplier != null) {
+                    final T t = supplier.get();
+                    if (t != null) {
+                        return of(t);
+                    }
+                }
+            }
+        }
+        return empty();
+    }
 
     public static final String L10N_BASEDIR_KEY = "l10n.basedir";
 
@@ -44,18 +73,18 @@ public class SimpleI18n<SELF extends SimpleI18n<SELF>> implements I18n {
 
     protected final Set<String> bundleBaseNames = new LinkedHashSet<>(4);
 
-    /** Can be {@code null} */
     protected String bundlePrefix;
 
-    /** Can be {@code null} */
     protected Locale locale;
 
-    /** Can be {@code null} */
     protected BiFunction<String, Locale, MessageFormat> messageFormatSupplier = memoize(MessageFormat::new);
 
-    /** Can be {@code null} */
     protected BiFunction<String, Locale, ResourceBundle> resourceBundleSupplier =
         (baseName, locale) -> locale == null ? getBundle(baseName) : getBundle(baseName, locale);
+
+    protected DateTimeFormatter dateTimeFormatter = RFC_1123_DATE_TIME;
+
+    protected ZoneId zoneId = ZoneId.systemDefault();
 
     public SimpleI18n(String... bundleBaseNames) {
         setBundlePrefix(getProperty(L10N_BASEDIR_KEY, L10N_BASEDIR_DEF).trim());
@@ -104,21 +133,33 @@ public class SimpleI18n<SELF extends SimpleI18n<SELF>> implements I18n {
         return self();
     }
 
+    public SELF setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
+        return self();
+    }
+
+    public SELF setZoneId(ZoneId zoneId) {
+        this.zoneId = zoneId;
+        return self();
+    }
+
     @Override
     public Locale getLocale() { return locale; }
+
+    @Override
+    public DateTimeFormatter getDateTimeFormatter() { return dateTimeFormatter; }
+
+    @Override
+    public ZoneId getZoneId() { return zoneId; }
 
     @NonNull
     @Override
     public String t(String key, Object... args) {
         if (key == null) {
-            log.debug("Cannot translate resource for a null key");
             return "";
         }
 
-        Locale locale = getLocale();
-        if (locale == null) {
-            locale = getDefaultLocale();
-        }
+        final Locale locale = firstNonNull(this::getLocale, this::getDefaultLocale).orElse(null);
 
         final String message = message(key, locale);
         if (args == null || args.length == 0 || messageFormatSupplier == null) {
@@ -157,13 +198,18 @@ public class SimpleI18n<SELF extends SimpleI18n<SELF>> implements I18n {
         return key;
     }
 
-    /**
-     * @see <a href="https://stackoverflow.com/a/23895571/272939">Answer to <em>Fluent API with inheritance and
-     *     generics</em></a>
-     * @see <a href="https://stackoverflow.com/a/7355094/272939">Is there a way to refer to the current type with a type
-     */
-    @SuppressWarnings("unchecked")
-    protected final SELF self() {
-        return (SELF) this;
+    @NonNull
+    @Override
+    public String l(Instant instant) {
+        if (instant == null) {
+            return "";
+        }
+
+        final DateTimeFormatter dtf =
+            firstNonNull(this::getDateTimeFormatter, this::getDefaultDateTimeFormatter).orElse(RFC_1123_DATE_TIME);
+        final Locale locale = firstNonNull(this::getLocale, this::getDefaultLocale).orElseGet(Locale::getDefault);
+        final ZoneId zoneId = firstNonNull(this::getZoneId, this::getDefaultZoneId).orElseGet(ZoneId::systemDefault);
+
+        return dtf.withLocale(locale).format(instant.atZone(zoneId));
     }
 }
