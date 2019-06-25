3a4c3ef6540f100d4bab9b1cd8a7fbeb13b2807c
Octavian Nita <octavian.nita@gmail.com>
2019-06-25 19:39:19 +0200

    Touch ErrorReport

--- sqlrose-core/src/main/java/net/appfold/sqlrose/core/error/ErrorReport.java
+++ sqlrose-core/src/main/java/net/appfold/sqlrose/core/error/ErrorReport.java
@@ -3,6 +3,9 @@ package net.appfold.sqlrose.core.error;
 import static java.util.Arrays.asList;
 
 /**
+ * {@link #add(Throwable...) Accumulates} error messages and eventual details and {@link #report() reports} them to the
+ * application user/client.
+ *
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
  * @version 1.0, Jun 21, 2019
  */
@@ -10,7 +13,9 @@ public interface ErrorReport {
 
     void report();
 
-    void addGenericReport();
+    void addGenericMessage();
+
+    void add(Object message, Object ...details);
 
     void add(Iterable<? extends Throwable> exceptions);
 
